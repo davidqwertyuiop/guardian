65a72f3c5ff8e4da095009d024632a680903e47a
davidqwertyuiop39 <davidqwertyuiop39@gmail.com>
2026-07-20 18:07:34 -0500

    modified:   apps/backend/src/infrastructure/sms/sendchamp.rs

--- apps/backend/src/infrastructure/sms/sendchamp.rs
+++ apps/backend/src/infrastructure/sms/sendchamp.rs
@@ -43,7 +43,6 @@ impl SendchampSmsService {
                 code
             ),
             "sender_name": self.sender,
-            "route": self.route,
         });
 
         let response = self
